71c32a9155665b176c9e7c484757122c8339a61b
chucky <charles.ssekitto@cobblestoneenergy.com>
2022-12-24 11:19:50 +0300

    add random choice picker

---
+++ random_choice_picker/script.js
@@ -0,0 +1,64 @@
+const tagsEl = document.getElementById("tags");
+const textArea = document.getElementById("textarea");
+
+textArea.focus();
+
+textArea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+
+  if (e.key === "Enter") {
+    setTimeout(() => {
+      e.target.value = "";
+    }, 10);
+    randomSelect();
+  }
+});
+
+function createTags(input) {
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+  tagsEl.innerHTML = "";
+
+  tags.forEach((tag) => {
+    const tagEl = document.createElement("span");
+    tagEl.classList.add("tag");
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl);
+  });
+}
+
+function randomSelect() {
+  const highLightTimes = 30;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+    highLightTag(randomTag);
+
+    setTimeout(() => {
+      unHighLightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+      highLightTag(randomTag);
+    }, 100);
+  }, highLightTimes * 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highLightTag(tag) {
+  tag.classList.add("highlight");
+}
+function unHighLightTag(tag) {
+  tag.classList.remove("highlight");
+}
