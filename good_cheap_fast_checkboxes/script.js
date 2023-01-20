0383ae58a810e2565de0fdd37b987794fa1021d7
chucky <charles.ssekitto@cobblestoneenergy.com>
2023-01-20 23:17:22 +0300

    add the styled check boxes

---
+++ good_cheap_fast_checkboxes/script.js
@@ -0,0 +1,25 @@
+const toggles = document.querySelectorAll(".toggle");
+const good = document.querySelectorAll("#good");
+const cheap = document.querySelectorAll("#cheap");
+const fast = document.querySelectorAll("#fast");
+
+console.log(toggles);
+toggles.forEach((toggle) =>
+  toggle.addEventListener("change", (e) => doTheTrick(e.target))
+);
+
+function doTheTrick(theClickedOne) {
+  if (good.checked && cheap.checked && fast.checked) {
+    if (good === theClickedOne) {
+      fast.checked = false;
+    }
+
+    if (cheap === theClickedOne) {
+      good.checked = false;
+    }
+
+    if (fast === theClickedOne) {
+      cheap.checked = false;
+    }
+  }
+}
